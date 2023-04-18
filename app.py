70bd29b5f244f1bd62a3a91075706a64f39ec84c
Hettore Eduardo <duh.barretoo@gmail.com>
2023-04-18 20:28:24 -0300

    Implementado template com css simples

--- app.py
+++ app.py
@@ -6,10 +6,22 @@ app = Flask(__name__)
 
 frase = "Hoje meu time joga. Que tristeza."
 
+produto = {
+	"id":1,
+	"nome":"Iphone 14 PRO ",
+	"descricao":"Iphone 14, modelo PRO MAX, na cor vermelha",
+	"preco":7800.00,
+	"imagem":"https://upload.wikimedia.org/wikipedia/commons/3/37/Back_of_the_iPhone_14_Pro.jpg"
+}
+
 @app.route("/")
 def deu_certo():
     #Estamos retornando o arquivo HTML já renderizado
     return render_template('exemplo.html', textinho = frase)
+
+@app.route("/produto")
+def exibe_produto():
+    return render_template("produto.html", **produto)
 
 app.run(debug=True)
 
